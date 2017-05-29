e4acd6d41c2932eec8b8537f52c15fc9353c0f6f
thelaserbeam17 <lazar_a@shaw.ca>
2017-05-29 16:03:05 -0700

    Add files via upload

--- TDR_water_trees.m
+++ TDR_water_trees.m
@@ -5,6 +5,7 @@ clc;
 %% Local Water Tree Model
 e_0 = 8.85e-12;
 f = 60;                                     % Hz
+w = 2*pi*f;                                 % rad/s
 cond_water = 1e-7;                          % conductivity of water
 e_water = 81-i*cond_water/(2*pi*f*e_0);     % complex permiativity of water
 e_xlpe = 2.3-i*.001;                        % complex permiativity of XLPE
@@ -22,11 +23,30 @@ e_total = w*e_wt + (1-w)*e_xlpe;            % total permiativity of water treed 
 %% Single Core Transmission Line Model
 cond_cu = 5.96e7;                           % conductivity of copper
 u_0 = (4*pi)*1e-7 ;                         % permeability of free space
-r_in = 1;                                   % radius of conductor
-r_out = 2;                                  % radius of insulation
+r_in = 5e-3;                                   % radius of conductor
+r_out = 100e-3;                                  % radius of insulation
 C_0 = 2*pi*e_0/log(r_out/r_in);             % geometric capcitance
-C_ins_eff = 2*pi*e_0*e_total/log(r_out/r_in);   % complex capacitance
-C_ins = real(C_ins_eff);                     % capacitance of insulation for single core cable (F/m)
-L = u_0/C_0;                                 % Inductance of conductor (H/m)
+
+% Degraded Cable Section
+C_deg_cmplx = 2*pi*e_0*e_total/log(r_out/r_in);   % complex capacitance
+C_ins_deg = real(C_deg_cmplx);                     % capacitance of insulation for single core cable (F/m)
+L_deg = u_0*e_0/C_0;                                 % Inductance of conductor (H/m)
+R_deg = 1/(pi*r_in^2*cond_cu);                  % Resistance of conductor (ohms/m)
+G_deg = real(2*pi*f*C_ins_deg);                     % Conductance of insulation (S/m)
+
+% Good condition Section
+C_cmplx = 2*pi*e_0*e_xlpe/log(r_out/r_in);   % complex capacitance
+C_ins = real(C_cmplx);                     % capacitance of insulation for single core cable (F/m)
+L = u_0*e_0/C_0;                                 % Inductance of conductor (H/m)
 R = 1/(pi*r_in^2*cond_cu);                  % Resistance of conductor (ohms/m)
 G = real(2*pi*f*C_ins);                     % Conductance of insulation (S/m)
+
+% Model Length 
+length = 100;                               % 100 meter cable
+deg_coef = .2;                              % 20 meters of cable is degraded due to water tree
+
+Z_good = (1-.2)*((R + i*w*L)/(G + i*w*C_ins))^.5;   % good condition cable chracteristic impedance (ohms)
+Z_deg = .2*((R + i*w*L)/(G + i*w*C_ins))^.5;        % degraded cables characteristic impedance     (ohms)
+
+Z_L = 50;                                           % terminating load (ohms)
+
