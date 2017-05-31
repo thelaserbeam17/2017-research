b396dd4dcd7c921e637205e6156a428fbd8e16b9
thelaserbeam17 <lazar_a@shaw.ca>
2017-05-31 15:58:13 -0700

    Add files via upload

--- TDR_water_trees.m
+++ TDR_water_trees.m
@@ -6,7 +6,7 @@ clc;
 e_0 = 8.85e-12;
 c = 3e8;                                    % speed of light m/s
 f = 60;                                     % Hz
-w = 2*pi*f;                                 % rad/s
+w = 2*pi*f;                             % rad/s
 cond_water = 1e-7;                          % conductivity of water
 e_water = 81-i*cond_water/(2*pi*f*e_0);     % complex permiativity of water
 e_xlpe = 2.3-i*.001;                        % complex permiativity of XLPE
@@ -18,8 +18,9 @@ q_w = kw*hw;                                % water content in water tree
 D = 1/4;                                    % geometry of water tree
 e_wt = e_xlpe*(1+q_w*(e_water-e_xlpe)/(e_xlpe+D*(1-q_w)*(e_water-e_xlpe))); % water tree permeativity
 
-w = .2;                                     % length parameter of water treed region
-e_total = w*e_wt + (1-w)*e_xlpe;            % total permiativity of water treed region
+x = .22;                                     % length parameter of water treed region
+%e_total = w*e_wt + (1-w)*e_xlpe;            % total permiativity of water treed region
+e_total = 1/(x/e_wt + (1-x)/e_xlpe);
 
 %% Single Core Transmission Line Model
 cond_cu = 5.96e7;                           % conductivity of copper
@@ -44,12 +45,12 @@ G = real(2*pi*f*C_ins);                     % Conductance of insulation (S/m)
 
 %% Model  
 length = .01;                               % 1 centimeter PUL set up
-deg_coef = .5;                              % 20 meters of cable is degraded due to water tree
+deg_coef = .9;                              % Governs what percent of cable is degrade
 
 Z_good = ((R + i*w*L)/(G + i*w*C_ins))^.5;   % good condition cable chracteristic impedance (ohms)
 Z_deg = ((R_deg + i*w*L_deg)/(G_deg + i*w*C_ins_deg))^.5;        % degraded cables characteristic impedance     (ohms)
 
-Z_L = 50;                                           % terminating load (ohms)
+Z_L = 1e100;                                           % terminating load (ohms)
 
 v_p = 1/(L*C_ins)^.5;                           % propagation velocity of good condition
 v_p_deg = 1/(L_deg*C_ins_deg)^.5;               % propagation velocity of water treed region
@@ -95,4 +96,66 @@ end
 Z_trans = (T(1,2)/T(2,1))^.5;                                 % overall characteristic impedance of line
 v_trans = length * 1/(imag(T(1,2))*imag(T(2,1))/w^2)^.5 * N;      % overall propagation velocity of line
 gamma_trans = 1/length * (T(1,2)*T(2,1))^.5 / N;                  % overall propagation constant of line
+
+
+% Trasnmission Line (Sensor at begining)
+trans_length = N*length;                                        % length of line in meters
+
+ref_1 = (Z_deg - Z_good)/(Z_deg + Z_good);                        % Reflection coefitient at first boandary
+ref_end = (Z_L - Z_good)/(Z_L + Z_good);                          % Reflection coefitient at end of cable
+
+t_p = trans_length/v_trans;                                     % time of pusle propegation along one lenght of cable 
+
+t_ref_1 = trans_length*(1-deg_coef)/v_trans;                    % time to reflection
+
+time = linspace(0, t_p, N);                                       % time vector
+V_p = zeros(1, N);
+V_in = zeros(1, N);
+V_tr = zeros(1, N);
+V_ref = zeros(1, N);
+
+noise = wgn(N, 1, 0);
+
+% input pulse
+for k = 1:N
+    if(time(k) <= 5e-9)
+        V_in(k) = 240 + noise(k);
+    end
+    if(time(k) > 5e-9)
+        V_in(k) = 0 + noise(k);
+    end 
+end
+figure;
+plot(time, V_in);
+
+distance = linspace(0, trans_length, N);
+V_p(1) = V_in(1);
+ref_occured = 0;
+
+for k = 1:N
+    if(x(k) == 1 && ref_occured ~= 2)
+        ref_occured = 1;
+    end
+    if(x(k) ~= 1)
+        ref_occured = 0;
+    end
+    
+    if(ref_occured ~= 1 && k ~= N)
+        V_p(k+1) = V_p(k)*exp(-gamma_trans*distance(k));
+        V_ref(k) = noise(k);
+    end
+    if(ref_occured == 1)
+        V_ref(k) = ref_1*V_p(k) + noise(k);
+        V_p(k+1) = V_p(k)*(1+ref_1);
+        ref_occured = 2;
+    end
+    if(k == N)
+        V_ref(k) = ref_end*V_p(k) + noise(k);
+    end
+end
+
+figure;
+plot(v_trans*time, V_ref);
+figure;
+plot(v_trans*time, V_p);
 
